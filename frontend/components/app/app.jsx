f8eea518e7dae696a06d0c7c29b5cb21a2eceab7
s.potaychuk92@gmail.com <mara4ivka>
2018-10-31 10:39:39 +0200

    add react & redux dependency

---
+++ frontend/components/app/app.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import AppTitle from 'arui-feather/app-title'
+import AppContent from 'arui-feather/app-content'
+import AuthForm from '../authform/authform'
+import Footer from 'arui-feather/footer'
+import Header from 'arui-feather/header'
+import Heading from 'arui-feather/heading'
+import Page from 'arui-feather/page'
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Page header={ <Header />} footer={<Footer />} >
+                <AppTitle>
+                    <Heading>Тестовый пример</Heading>
+                </AppTitle>
+                <AppContent>
+                    <AuthForm/>
+                </AppContent>
+            </Page>
+        );
+    }
+}
+
+export default App;
